e651b5509dbee8890799796ed726821829eeed4e
wreckitralphhh <ralphdelap27@gmail.com>
2025-12-11 21:16:56 -0800

    Ok this is probably the final one

--- Peel Mechanism/data_plotter.py
+++ Peel Mechanism/data_plotter.py
@@ -1,15 +1,13 @@
 # There are 50,000 iterations in the raw file and that way too much to look at 
 # Let's divide the 50K into chunks and take a look at each "checkpoint"
-# Let's say every "checkpoint" == 10K iterations and we will look at the 10Kth plot
+# Let's say every checkpoint = 10K iterations and we will look at the 10Kth plot
+# We can still make conclusions about the performance of the product if we're only plotting 5 lines 
 
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-#loads in CSV, make sure you use a raw string or \\ on windows 
-#importing Path and using it doesn't work for some reason
+#loads in CSV, prints top of dataset to make sure it arrived safely
 df = pd.read_csv(r"C:\Users\Ralph\Desktop\test_engineer_data.csv")
-
-#prints top of dataset, just making sure it arrived safely
 print(df.head())
 
 #this will let us group the dataset, by iteration 
@@ -21,16 +19,17 @@ group_idx_names = ['0 Iterations', '10000 Iterations', '20000 Iterations', '3000
 # I locked the "relative time" of each test since I kept seeing that 8.13 seconds is the same duration of each test iteration all the way to 49999th 
 # This makes it easier to plot using the same x-axis 
 rel_time = groups[0]['Timestamp (s)']
-plt.figure(figsize=(12,5))
+plt.figure(figsize=(20,8))  #Sorry if you have small screen
 
 for i, idx in enumerate(group_indices):
     g = groups[idx].copy()
-    print(i)
-    print(idx)
+    # print(i)
+    # print(idx)
     encoder_motion = groups[idx]['Encoder (deg)']
     plt.scatter(rel_time, encoder_motion)
-    plt.plot(rel_time, encoder_motion)
+    plt.plot(rel_time, encoder_motion, marker='o', linestyle='-')
 
+plt.title("Encoder Postion over ")
 plt.xlabel("Time Elapsed (sec)")
 plt.ylabel("Encoder Position (deg)")
 plt.legend(labels=group_idx_names, loc='center left', bbox_to_anchor=(1,0.5))
