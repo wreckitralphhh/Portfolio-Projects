70f7c7dcdb211016cf080cd7c263292bdab84bc9
wreckitralphhh <ralphdelap27@gmail.com>
2025-12-11 21:38:39 -0800

    Added grid to the generated plot

--- Peel Mechanism/data_plotter.py
+++ Peel Mechanism/data_plotter.py
@@ -1,7 +1,7 @@
 # There are 50,000 iterations in the raw file and that way too much to look at 
 # Let's divide the 50K into chunks and take a look at each "checkpoint"
 # Let's say every checkpoint = 10K iterations and we will look at the 10Kth plot
-# We can still make conclusions about the performance of the product if we're only plotting 5 lines 
+# We can still make conclusions about the performance of the product if we're only plotting the data at these checkpoints  
 
 import pandas as pd 
 import matplotlib.pyplot as plt
@@ -11,8 +11,8 @@ df = pd.read_csv(r"C:\Users\Ralph\Desktop\test_engineer_data.csv")
 print(df.head())
 
 #this will let us group the dataset, by iteration 
-groups = [g for _, g in df.groupby(df.index // 20)] #Every 20 rows, it's a new iteration number 
-group_indices = [0, 10000, 20000, 30000, 40000, 49999]  # the 5 groups
+groups = [g for _, g in df.groupby(df.index // 20)] # Every 20 rows, it's a new iteration index 
+group_indices = [0, 10000, 20000, 30000, 40000, 49999]  # These are the "checkpoints" I want to look at 
 group_idx_names = ['0 Iterations', '10000 Iterations', '20000 Iterations', '30000 Iterations', '40000 Iterations', '49999 Iterations']
 # print(groups[20000]['Iteration']) # I was just making sure it was grouped correctly
 
@@ -21,15 +21,13 @@ group_idx_names = ['0 Iterations', '10000 Iterations', '20000 Iterations', '3000
 rel_time = groups[0]['Timestamp (s)']
 plt.figure(figsize=(20,8))  #Sorry if you have small screen
 
-for i, idx in enumerate(group_indices):
-    g = groups[idx].copy()
-    # print(i)
-    # print(idx)
+# Plots each iteration using a for loop, using the same x-axis
+for i, idx in enumerate(group_indices):   
     encoder_motion = groups[idx]['Encoder (deg)']
-    plt.scatter(rel_time, encoder_motion)
     plt.plot(rel_time, encoder_motion, marker='o', linestyle='-')
 
-plt.title("Encoder Postion over ")
+plt.grid(True)
+plt.title("Encoder Postion Through Iterations")
 plt.xlabel("Time Elapsed (sec)")
 plt.ylabel("Encoder Position (deg)")
 plt.legend(labels=group_idx_names, loc='center left', bbox_to_anchor=(1,0.5))
